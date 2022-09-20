bedc40e47240bb6d46b0f4702046b73fcdfef95f
Kaylie Phillips <kaylie.travel@gmail.com>
2022-09-19 20:56:52 -0500

    Completed todo list

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ArraysAndLists
 {
@@ -8,12 +11,16 @@ namespace ArraysAndLists
         {
             //TODO:
 
-            // Create an int Array and populate numbers 1-10
+            // DONE Create an int Array and populate numbers 1-10
+            int[] myArray = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-            /* Create two Lists of type int.
+            /* DONE Create two Lists of type int.
              * Name one List "evens"
              * Name the other List "odds"
              */
+
+            List<int> evens = new List<int>();
+            List<int> odds = new List<int>();
 
             /* Using either a foreach or for loop,
              * nest an if statement to check to see
@@ -22,11 +29,44 @@ namespace ArraysAndLists
              * or the odds List
              */
 
+            foreach (int num in myArray)
+            {
+                if (num%2 == 0)
+                {
+                    evens.Add(num);
+                }
+                else
+                {
+                    odds.Add(num);
+                }
+            }
+
+
             /* Now using foeach or for loops,
              * display each List of even and odd numbers
              *
              * Try to be creative in your display
              */
+            Console.Write($"This is the entire list of numbers: ");
+            foreach (int num in myArray)
+            {
+                Console.Write($"{num} ");
+            }
+            Console.WriteLine();
+
+            Console.Write($"These are the even numbers from the list: ");
+            foreach (int num in evens)
+            {
+                Console.Write($"{num} ");
+            }
+            Console.WriteLine();
+
+            Console.Write($"These are the odd numbers from the list: ");
+            foreach (int num in odds)
+            {
+                Console.Write($"{num} ");
+            }
+            
         }
     }
 }
